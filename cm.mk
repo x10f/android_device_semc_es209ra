7f60ef13246f31151bbc25ccc2a740bc4ad8ff66
x10forevers <android.x10f@gmail.com>
2014-12-26 19:59:58 -0400

    MoKee bringup

--- cm.mk
+++ cm.mk
@@ -1,8 +1,8 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/semc/es209ra/full_es209ra.mk)
@@ -16,7 +16,7 @@ PRODUCT_RELEASE_NAME := X10i
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := es209ra
-PRODUCT_NAME := cm_es209ra
+PRODUCT_NAME := mk_es209ra
 
 #Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=LT18i BUILD_FINGERPRINT="SEMC/LT18i_1254-2184/LT18i:4.0.4/4.1.B.0.431/UL5_3w:user/release-keys" PRIVATE_BUILD_DESC="LT18i-user 4.0.4 4.1.B.0.431 UL5_3w test-keys"
